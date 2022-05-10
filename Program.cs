b273055092f9e6bb16d1beb4bfa5b75323e1c5f2
anti.niko <anti.niko@ya.ru>
2022-05-10 14:07:52 +0300

    final done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[7] { "Russia", "050", "2022", "hello", ":-)", "world", "-2" };
+string[] array2 = new string[array1.Length];
+Console.Write($"Новый сформированный массив: [");
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+       
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($"]");
+}
+FillArray(array1, array2);
+PrintArray(array2);
